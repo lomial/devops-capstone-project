3b5c46e3b7d3a8ed767d03847524dd547786de9d
lomial <34379749+lomial@users.noreply.github.com>
2026-03-15 09:18:35 +0300

    Add test case for reading an account
    Added a test case to verify reading an account.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,17 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+    account = self._create_accounts(1)[0]
+
+    resp = self.client.get(
+        f"/accounts/{account.id}",
+        content_type="application/json"
+    )
+
+    self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    data = resp.get_json()
+
+    self.assertEqual(data["name"], account.name)
+    self.assertEqual(data["email"], account.email)
